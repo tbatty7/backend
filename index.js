c37a1621fc65aafe8a49f5e717d61e9fa729266e
Tim Batty <tbatty7@gmail.com>
2025-01-28 13:33:35 -0500

    refactored to function

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
-  res.json({message: 'Hi Mom'});
+  res.json({ message: "Hi Mom" });
 });
+
 
 app.post("/path", (req, res) => {
   console.log(req.body);
-  res.json({message: "hi " + req.body.name });
+  res.json({ response: fizzBuzz(req.body.numbers) });
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+function fizzBuzz(numbers) {
+  return numbers;
+}
+
